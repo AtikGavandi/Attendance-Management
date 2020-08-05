d57efe2610b30fe067d8aa7a905491d4fd52f2d8
atik.gavandi27@outlook.com <atik.gavandi27@outlook.com>
2020-08-05 14:29:59 +0530

    Student Controller added and created list view of student

---
+++ AttendanceManagement/AttendanceManagement/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using DAL;
+using DAL.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AttendanceManagement.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+
+            List<Student> Students = new List<Student>();
+            try
+            {
+                using (var unitOfWork = new UnitOfWork(new DBEntities()))
+                {
+                    Students = unitOfWork.Students.GetAll().OrderBy(x => x.Id).ToList();
+                }
+
+                ViewBag.Students = Students;
+                return View();
+            }
+
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
+
+
+        }
+    }
+}
